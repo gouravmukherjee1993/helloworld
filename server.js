483d6bc6f83c57234aa81555eabb557f0c4d170d
Gourav Mukherjee <66010797+gouravmukherjee1993@users.noreply.github.com>
2021-01-08 21:30:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,6 +28,21 @@ app.get('/data.wsdl', function(req, res) {
 	// ejs render automatically looks in the views folder
 	//res.render('index');
 });
+app.get('/ImageUploadService.xsd', function(req, res) {
+	res.contentType("text/xml");
+//	res.sendFile(__dirname + '/public/wsdl/data.wsdl');
+//	res.send('Hello World');
+
+	fs.readFile(__dirname + '/public/wsdl/data.wsdl', 'utf8' , (err, data) => {
+		if (err) {
+		  console.error(err)
+		  return
+		}
+		res.send(data);
+	  })
+	// ejs render automatically looks in the views folder
+	//res.render('index');
+});
 
 app.listen(port, function() {
 	console.log('Our app is running on http://localhost:' + port);
